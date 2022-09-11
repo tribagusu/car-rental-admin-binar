eeb2586663b5f002742a5b9552670a748f7e8632
tribagus <tri.bagus21@gmail.com>
2022-09-11 13:31:45 +0700

    add base styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from "react"
+import "./index.scss"
+
 function App() {
-  return <div></div>
+  return (
+    <div className="row">
+      <div className=" d-f col-12-xs justify-center gap-2">
+        <h1>tes</h1>
+        <h1>tes</h1>
+        <h1>tes</h1>
+      </div>
+    </div>
+  )
 }
 
 export default App
