f94c0b996ad5acbab62f00d256bcf13d05feb1f4
tribagus <tri.bagus21@gmail.com>
2022-09-11 16:51:37 +0700

    add signin component and login page

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import cover from "../assets/cover-login.png"
 
 const SignIn = (props) => {
   const { handleEmail, handlePassword, email, password, handleSubmit } = props
@@ -7,6 +8,11 @@ const SignIn = (props) => {
     <>
       <div className="signin">
         <div className="left">
+          <div className="signin-right-image">
+            <img src={cover} alt="cover-login" />
+          </div>
+        </div>
+        <div className="right">
           <div className="signin-container">
             <div className="logo">
               <Link to="/">
@@ -16,35 +22,19 @@ const SignIn = (props) => {
             <h2>Welcome Back!</h2>
             <form onSubmit={handleSubmit}>
               <div className="signin-email">
-                <div className="label">
-                  <label>Email</label>
-                </div>
+                <label>Email</label>
                 <input onChange={handleEmail} value={email} type="text" />
               </div>
               <div className="signin-password">
-                <div className="signin-label">
-                  <label>Password</label>
-                </div>
+                <label>Password</label>
                 <input
                   onChange={handlePassword}
                   value={password}
                   type="password"
                 />
               </div>
-              <button className="btn">Sign In</button>
+              <button className="btn-primary">Sign In</button>
             </form>
-            <div className="signin-signup">
-              <p>Don't have an account?</p>
-              <Link to="/registration">Sign Up for free</Link>
-            </div>
-          </div>
-        </div>
-        <div className="right">
-          <div className="signin-right-logo">
-            <h1>Binar Car Rental</h1>
-          </div>
-          <div className="signin-right-image">
-            <img src="" alt="image" />
           </div>
         </div>
       </div>
