184ce8e3ef70d3900d7ae1fba6a69cc53b39a502
tribagus <tri.bagus21@gmail.com>
2022-09-16 23:29:36 +0700

    add card components for car list

--- src/components/Car/CarList/Card/index.js
+++ src/components/Car/CarList/Card/index.js
@@ -3,7 +3,21 @@ import React from "react"
 const CarCard = () => {
   return (
     <section className="car-card-container">
-      <h1>card</h1>
+      <div className="car-card">
+        <div className="card-body">
+          <div className="card-image"></div>
+          <p>Nama/Tipe Mobil</p>
+          <h4>Rp 500.000 / hari</h4>
+          <p>6 - 8 people</p>
+          <p>updated time</p>
+          <div className="card-button">
+            <button className="button-delete btn-outlined-danger">
+              Delete
+            </button>
+            <button className="button-edit btn">Edit</button>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
