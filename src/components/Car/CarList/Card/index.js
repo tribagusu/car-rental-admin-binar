5604bab5d63bf3f476ed410bb1d8675de538d8eb
tribagus <tri.bagus21@gmail.com>
2022-09-17 11:39:18 +0700

    updated card component and car list

--- src/components/Car/CarList/Card/index.js
+++ src/components/Car/CarList/Card/index.js
@@ -1,8 +1,21 @@
 import React, { useEffect } from "react"
 import { useSelector, useDispatch } from "react-redux"
+
+// func
 import { handleCars } from "../../../../redux/actions/carsAction"
-import moment from "moment/moment"
 import { formatCurrency } from "../../../../utils/formatCurrency"
+
+// library
+import moment from "moment/moment"
+import {
+  UilUsersAlt,
+  UilClock,
+  UilTrash,
+  UilEdit,
+} from "@iconscout/react-unicons"
+
+// asset
+import noImage from "../../../../assets/no-image.png"
 
 const CarCard = () => {
   const { cars } = useSelector((state) => state.cars)
@@ -14,25 +27,40 @@ const CarCard = () => {
 
   const car = cars.map((car) => car)
   const timeUpdated = moment(car.updatedAt).format("MMM D YYYY, hh:mm")
-  //   const carPrice = formatCurrency(car.price)
 
   return (
     <section className="car-card-container">
       {cars.map((car) => (
         <div className="car-card">
-          <div className="card-body">
-            <img src={car.image} alt="car" />
-            <p>{car.name}</p>
-            <h4>{`${formatCurrency(car.price)} / hari`}</h4>
-            <p>{car.category}</p>
-            <small>Updated at {timeUpdated}</small>
-            <div className="card-button">
-              <button className="button-delete btn-outlined-danger">
-                Delete
-              </button>
-              <button className="button-edit btn">Edit</button>
+          <>
+            <div className="card-body">
+              <div className="card-image">
+                <img src={car.image ? car.image : noImage} alt="car" />
+              </div>
+              <div className="card-content">
+                <p>{car.name}</p>
+                <h4>{`${formatCurrency(car.price)} / hari`}</h4>
+                <div className="car-category">
+                  <UilUsersAlt />
+                  <p>{car.category}</p>
+                </div>
+                <div className="car-updated">
+                  <UilClock />
+                  <small>Updated at {timeUpdated}</small>
+                </div>
+                <div className="card-button">
+                  <button className="button-delete btn-outlined-danger">
+                    <UilTrash />
+                    <p>Delete</p>
+                  </button>
+                  <button className="button-edit btn">
+                    <UilEdit />
+                    <p>Edit</p>
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
+          </>
         </div>
       ))}
     </section>
