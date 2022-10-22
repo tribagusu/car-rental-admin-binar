378591a73f2077cec6a473faedd5c8c58c071d15
tribagus <tri.bagus21@gmail.com>
2022-10-22 16:14:34 +0700

    change page show to 6 and update css cars page

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,7 +2,7 @@ import access_token from "./accessToken"
 
 export const getCars = async (page, category) => {
   const res = await fetch(
-    `https://bootcamp-rent-cars.herokuapp.com/admin/v2/car?category=${category}&page=${page}&pageSize=9`,
+    `https://bootcamp-rent-cars.herokuapp.com/admin/v2/car?category=${category}&page=${page}&pageSize=6`,
     {
       headers: {
         access_token,
