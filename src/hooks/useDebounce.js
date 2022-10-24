45c198fefab2a0fa9fb572488010502a7428a28f
tribagus <tri.bagus21@gmail.com>
2022-10-24 21:35:39 +0700

    add debounce function

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react"
+
+export default function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+
+    return () => clearTimeout(timeoutId)
+  }, [value])
+
+  return debouncedValue
+}
